407c0bba1950b4e80c94bf8d4273c0c1002a934e
floppy007 <fhesse@comnic.eu>
2026-02-15 02:58:40 +0100

    Add favicon using logo.png

--- install.php
+++ install.php
@@ -67,6 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="icon" type="image/png" href="logo.png">
 <title>TaskFlow - Installation</title>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <style>
